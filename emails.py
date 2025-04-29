5bfd11dc80b334f45dd398f1c427ac9a54fa0c82
Joshua Nelson <jnelson160@my.gcu.edu>
2025-04-29 13:01:23 -0500

    initialized email sending

---
+++ emails.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv # type: ignore
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Email details
+sender = os.getenv("SENDER_EMAIL")
+recipient = os.getenv("RECIPIENT_EMAIL")
+subject = "Hello World Subject Line!"
+message_body = "THIS IS A TEST MESSAGE"
+
+# Create the email
+msg = MIMEMultipart()
+msg['From'] = sender
+msg['To'] = recipient
+msg['Subject'] = subject
+msg.attach(MIMEText(message_body, 'plain'))
+
+# Get password from environment variable
+password = os.getenv("APP_PASSWORD")
+
+if not password:
+    raise ValueError("APP_PASSWORD not set in environment variables")
+
+# Send the email
+with smtplib.SMTP("smtp.mail.me.com", 587) as server:
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(sender, password)
+    server.send_message(msg)
